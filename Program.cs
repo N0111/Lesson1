5e56cd263e92d8723ccd4e635e99085f8b87e6d3
Nastya K <nastulya01112005@gmail.com>
2021-12-11 22:42:44 +0200

    Task1-2

--- Program.cs
+++ Program.cs
@@ -7,20 +7,22 @@ string name = Console.ReadLine();
 Console.WriteLine("Введите фамилию: ");
 string surname = Console.ReadLine();
 Console.WriteLine("Введите возраст: ");
-string age = Console.ReadLine();
-Console.WriteLine("Введите рост: ");
-string height = Console.ReadLine();
-Console.WriteLine("Введите вес: ");
-string weight = Console.ReadLine();
-Console.WriteLine(" Здравствуйте " + surname + " " + name + " в ваши " + age + " рост " + height + " при весе " + weight + " абсолютно нормально");
+int age = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите рост(в метрах): ");
+double h = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите вес (в килограмах): ");
+double m = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine(" Здравствуйте " + surname + " " + name + " в ваши " + age + " рост " + h+ " при весе " + m + " абсолютно нормально");
 
 //б) используя форматированный вывод;
-Console.WriteLine(" Здравствуйте {0} {1} в ваши {2} рост {3} при весе {4} абсолютно нормально", surname, name, age, height, weight);
+Console.WriteLine(" Здравствуйте {0} {1} в ваши {2} рост {3} при весе {4} абсолютно нормально", surname, name, age, h, m);
 
 //в) используя вывод со знаком $.
-Console.WriteLine($" Здравствуйте {surname} {name} в ваши {age} рост {height} при весе {weight} абсолютно нормально");
+Console.WriteLine($" Здравствуйте {surname} {name} в ваши {age} рост {h} при весе {m} абсолютно нормально");
 
 // 2. Ввести вес и рост человека. Рассчитать и вывести индекс массы
 //  тела (ИМТ) по формуле I=m/(h*h); где m — масса тела в килограммах,
 //   h — рост в метрах.
+double i = m/(h*h);
+Console.WriteLine($"ИМТ = {i:F2}");
 
